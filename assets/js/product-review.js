7ba25feb5cda2678ffbc1b7d68f7aeb1fabcd85f
RubinoD1 <Rubinod1@gmail.com>
2024-08-20 23:28:00 -0400

    features & quality star ratings working in product-review.js

--- assets/js/product-review.js
+++ assets/js/product-review.js
@@ -1,10 +1,24 @@
 
+
+
 //star value event listeners 
 document.getElementById("value-1").onclick = function(){star("value" ,1);}
 document.getElementById("value-2").onclick = function(){star("value" ,2);}
 document.getElementById("value-3").onclick = function(){star("value" ,3);}
 document.getElementById("value-4").onclick = function(){star("value" ,4);}
 document.getElementById("value-5").onclick = function(){star("value" ,5);}
+// star quality event listeners
+document.getElementById("quality-1").onclick = function(){star("quality" ,1);}
+document.getElementById("quality-2").onclick = function(){star("quality" ,2);}
+document.getElementById("quality-3").onclick = function(){star("quality" ,3);}
+document.getElementById("quality-4").onclick = function(){star("quality" ,4);}
+document.getElementById("quality-5").onclick = function(){star("quality" ,5);}
+// star features event listeners
+document.getElementById("features-1").onclick = function(){star("features" ,1);}
+document.getElementById("features-2").onclick = function(){star("features" ,2);}
+document.getElementById("features-3").onclick = function(){star("features" ,3);}
+document.getElementById("features-4").onclick = function(){star("features" ,4);}
+document.getElementById("features-5").onclick = function(){star("features" ,5);}
 
 function star(id, value){
  for(let i = 1; i < 6; i++){
@@ -17,6 +31,8 @@ function star(id, value){
    }
  }
 
- //set name value to true in array 
-
+ //set name value to true in array & set number value  
+  
 }
+
+// valiation array 
