2a1be071f109f653ad761b069a4bfb3812eaac08
RubinoD1 <Rubinod1@gmail.com>
2024-08-20 23:09:38 -0400

    star rating working for value in product-review.js

--- assets/js/product-review.js
+++ assets/js/product-review.js
@@ -1,5 +1,22 @@
 
+//star value event listeners 
+document.getElementById("value-1").onclick = function(){star("value" ,1);}
+document.getElementById("value-2").onclick = function(){star("value" ,2);}
+document.getElementById("value-3").onclick = function(){star("value" ,3);}
+document.getElementById("value-4").onclick = function(){star("value" ,4);}
+document.getElementById("value-5").onclick = function(){star("value" ,5);}
 
-//change html unicode on click -- event listener 
+function star(id, value){
+ for(let i = 1; i < 6; i++){
+  let name = id;
+  let number = value;
+   if(i <= number){
+    document.getElementById(`${name}` + "-" + `${i}`).innerHTML = `&#x2605;`;//black star unicode
+   }else {
+    document.getElementById(`${name}` + "-" + `${i}`).innerHTML = `&#x2606;`;//white star unicode
+   }
+ }
 
- 
+ //set name value to true in array 
+
+}
